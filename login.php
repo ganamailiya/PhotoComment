b4d1659d692366e61d78635f45d8aa7be03c04e2
iliyaganama <1510537@rgu.ac.uk>
2016-04-28 21:17:36 +0100

    prepared stmt

--- login.php
+++ login.php
@@ -21,25 +21,25 @@
 			$password = mysqli_real_escape_string($db, $password );
 			$password = md5( $password );
 
-			// Check the database (if username matches the password)
+			/** Check the database (if username matches the password)
 			$data = $db->prepare( 'SELECT * FROM users WHERE username = (:username) AND password = (:password) LIMIT 1;' );
 			$data->bind_Param( ':username', $username, PDO::PARAM_STR);
 			$data->bind_Param( ':password', $password, PDO::PARAM_STR );
 			$data->execute();
-			$row = $data->fetch();
+			$row = $data->fetch(); **/
 
 
 
 			//Check username and password from database
-			//$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
-			//$result=mysqli_query($db,$sql);
-			//$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
+			$result=mysqli_query($db,$sql);
+			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
 			
 			//If username and password exist in our database then create a session.
 			//Otherwise echo error.
 			
-			//if(mysqli_num_rows($result) == 1)
-			if ( $data->rowCount() == 1 )
+			if(mysqli_num_rows($result) == 1)
+			//if ( $data->rowCount() == 1 )
 			{
 				$_SESSION['username'] = $username; // Initializing Session
 				header("location: photos.php"); // Redirecting To Other Page
