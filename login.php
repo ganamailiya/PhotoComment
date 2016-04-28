bb62e68475aab5bfa00adbfd603afa86ec4b1984
iliyaganama <1510537@rgu.ac.uk>
2016-04-28 20:59:09 +0100

    prepared stmt

--- login.php
+++ login.php
@@ -10,25 +10,41 @@
 			$error = "Both fields are required.";
 		}else
 		{
-			// Define $username and $password
+			// Sanitise password input
 			$username=$_POST['username'];
+			$username = stripslashes( $username );
+			$username = mysqli_real_escape_string( $username );
+
+			// Sanitise password input
 			$password=$_POST['password'];
+			$password = stripslashes( $password );
+			$password = mysqli_real_escape_string( $password );
+			$password = md5( $password );
+
+			// Check the database (if username matches the password)
+			$data = $db->prepare( 'SELECT * FROM users WHERE username = (:username) AND password = (:password) LIMIT 1;' );
+			$data->bindParam( ':username', $username, PDO::PARAM_STR);
+			$data->bindParam( ':password', $password, PDO::PARAM_STR );
+			$data->execute();
+			$row = $data->fetch();
 
 
-			
+
 			//Check username and password from database
-			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
-			$result=mysqli_query($db,$sql);
-			$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+			//$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
+			//$result=mysqli_query($db,$sql);
+			//$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
 			
 			//If username and password exist in our database then create a session.
 			//Otherwise echo error.
 			
-			if(mysqli_num_rows($result) == 1)
+			//if(mysqli_num_rows($result) == 1)
+			if ( $data->rowCount() == 1 )
 			{
 				$_SESSION['username'] = $username; // Initializing Session
 				header("location: photos.php"); // Redirecting To Other Page
-			}else
+			}
+			else
 			{
 				$error = "Incorrect username or password.";
 			}
