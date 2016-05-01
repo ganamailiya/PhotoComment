4f4971d3c853d65ae4740e72fc76c9ead06e2024
iliyaganama <1510537@rgu.ac.uk>
2016-05-01 09:06:47 +0100

    prepared stmt

--- submit.php
+++ submit.php
@@ -2,41 +2,46 @@
 include ("connection.php");
 $msg = "";
 if(isset($_POST["submit"])) {
-$name = $_POST["username"];
-$name = htmlentities($name);
-$name = stripslashes($name);
-$name = htmlspecialchars($name);
-$name = mysqli_real_escape_string($db, $name);
+    //define & sanitize name, email & password.
+    $name = $_POST["username"];
+    $name = htmlentities($name);
+    $name = stripslashes($name);
+    $name = htmlspecialchars($name);
+    $name = mysqli_real_escape_string($db, $name);
 
-$email = $_POST["email"];
-$email = htmlentities($email);
-$email = stripslashes($email);
-$email = htmlspecialchars($email);
-$email = mysqli_real_escape_string($db, $email);
+    //define & sanitize email
+    $email = $_POST["email"];
+    $email = htmlentities($email);
+    $email = stripslashes($email);
+    $email = htmlspecialchars($email);
+    $email = mysqli_real_escape_string($db, $email);
 
-$password = $_POST["password"];
-$password = mysqli_real_escape_string($db, $password);
-$password = password_hash($password, PASSWORD_BCRYPT);
+    //define & sanitize password
+    $password = $_POST["password"];
+    $password = mysqli_real_escape_string($db, $password);
+    $password = password_hash($password, PASSWORD_BCRYPT);
 
-$datam = $db->prepare('SELECT email FROM users WHERE email = ? ');
-$datam->bind_param('s', $email);
-$datam->execute();
-$rower = $datam->get_result();
-$rower = $rower->fetch_assoc();
+    //check if e-mail exists
+    $datam = $db->prepare('SELECT email FROM users WHERE email = ? ');
+    $datam->bind_param('s', $email);
+    $datam->execute();
+    $rower = $datam->get_result();
+    $rower = $rower->fetch_assoc();
 
-if($rower) {
-$msg = "Sorry...This email already exists...";
-}
-else
-{
-//echo $name." ".$email." ".$password;
-$query = $db->prepare("INSERT INTO users (username, email, password) VALUES ('?', '?', '?')");
-$query->bind_param('sss', $name, $password,$email);
+    if($rower) {
+        $msg = "Sorry...This email already exists...";
+    }
+    else
+    {
+        //echo $name." ".$email." ".$password;
+        $query = $db->prepare("INSERT INTO users (username, email, password) VALUES ('?', '?', '?')");
+        $query->bind_param('sss', $name, $password,$email);
 
-if($query->execute()) {
-$msg = "Thank You! you are now registered. click <a href='index.php'>here</a> to login";
-}
+        if($query->execute()) {
+            $msg = "Thank You! you are now registered. click <a href='index.php'>here</a> to login";
+        }
 
-}
+    }
+
 }
 ?>
