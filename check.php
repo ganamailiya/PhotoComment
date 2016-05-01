73f536fd3c681fc24ea5f390d6f613b9bfd4a006
iliyaganama <1510537@rgu.ac.uk>
2016-05-01 19:21:48 +0100

    prepared stmt

--- check.php
+++ check.php
@@ -22,7 +22,7 @@ if (isset($_SESSION['timeout'])) {
     $logintime = $_SESSION['timeout'];
     $differenceintime = time() - $logintime;
 
-    if ($differenceintime >= 5) { //session expiration
+    if ($differenceintime >= 300) { //session expiration
         session_unset();
         session_destroy();
         header("Location: index.php");
